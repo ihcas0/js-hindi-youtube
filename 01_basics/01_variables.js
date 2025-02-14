2b8560a2ec054cfc1e35b9d467c1001c13c9d8b3
ihcas0 <154008477+ihcas0@users.noreply.github.com>
2025-02-14 14:19:48 +0000

    Day 2 : Task Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 133859
+let accountEmail = "notmytype@gmail.com"
+var accountPassword = "13556"
+accountCity = "Jaipur"
+var accountState
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" // Treat all JS code as the newer version of the JS
+
+// There's a different syntax to use alert( 6 - 58) in the node normally can use  in html binding or console in inspect 
+// Arithmetic operations
+// Work on readability of the code
+// Documentation of MDN -> written by mozila and ecma is the real .... can read later 
+// null => standalone value ... very much required as say the temperature say asked then 0 isn't looked 
+// undefined => variable still not alone
+// symbol => unique ... much used in react
+// object
+
+console.log(typeof undefined) // undefined
+
+console.log(typeof null) // object
